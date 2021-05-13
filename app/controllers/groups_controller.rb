7e221384c34dd5449848d77b77c16ab1099e976f
appletea946 <shouyisheng@gmail.com>
2021-05-13 16:29:03 +0900

    グループ作成機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,29 @@
 class GroupsController < ApplicationController
   def index
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    binding.pry
+    @group = Group.new(group_params)
+    if @group.valid?
+      @group.save
+      render :index
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :details, :start_date, :end_date, :deadline_date, :max_num_of_people).merge(user_id: current_user.id)
+  end
 end
